4e0b5886ae213957eaac0554120ec57ca4da8872
qwerdf1133 <anwltjdwhdgus@gmail.com>
2024-01-08 12:23:06 +0900

    pay commit

--- src/dao/PayDaoImpl.java
+++ src/dao/PayDaoImpl.java
@@ -16,11 +16,18 @@ public class PayDaoImpl implements PayDAO {
 	@Override
 	public PayVO pay(PayVO payVO) {
 		Connection conn = DBUtil.getConnection();
-		String sql = "select name, date, seat, musical, price from pay where price";
+		String sql = "select name, date, seat, musical, price from pay";
 		try {
 			PayDAO m;
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, sql);
+			pstmt.setString(1, payVO.getName());
+			pstmt.setString(2, payVO.getDate());
+			pstmt.setString(3, payVO.getSeat());
+			pstmt.setString(4, payVO.getMusical());
+			pstmt.setString(5, payVO.getPrice());
+			
+			int result = pstmt.executeUpdate();
+			
 		}catch (SQLException e) {
 			
 		}finally {
